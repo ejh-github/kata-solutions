e24825a2d441abf9e8d9bc13dda5f632ce4fd101
Elias Howell <ejh@snarf.top>
2021-03-02 07:23:35 -0600

    Update ip.go

--- ip.go
+++ ip.go
@@ -1,27 +1,27 @@
 package kata
 
 import (
-  "fmt"
-  "strings"    
-  "strconv"
+        "fmt"
+        "strconv"
+        "strings"
 )
 
 func Is_valid_ip(ip string) bool {
-  check := strings.Split(ip, ".")
-  if len(check) != 4 {
-    return false
-  }
-  for i := 0; i < len(check); i++ {
-    comparison, err := strconv.Atoi(check[i])
-    fmt.Println(comparison)
-    if err != nil {
-      return false
-    }
-    if comparison >= 0 && comparison <= 255 {
-      fmt.Println("valid")
-    } else {
-      return false
-    }
-  }
-  return true
+        check := strings.Split(ip, ".")
+        if len(check) != 4 {
+                return false
+        }
+        for i := 0; i < len(check); i++ {
+                comparison, err := strconv.Atoi(check[i])
+                fmt.Println(comparison)
+                if err != nil {
+                        return false
+                }
+                if comparison >= 0 && comparison <= 255 {
+                        fmt.Println("valid")
+                } else {
+                        return false
+                }
+        }
+        return true
 }
